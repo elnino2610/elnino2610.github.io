59d6b4c41644d82b732bbc0848d2a0884a7fb88c
techmaster <techmaster@techmasters-Mac-mini-3.local>
2018-03-08 15:06:12 +0700

    dsadsa

--- Project/js/js.js
+++ Project/js/js.js
@@ -7,22 +7,28 @@
 
 // 		})
 
-function show(){
-	$('div.text-content').addClass('show');
-}
+// function show(){
+// 	$('div.text-content').addClass('show');
+// }
 
-$(window).scroll(function () {
-    var top=$(window).scrollTop();
-   if (top > 100) {
-     $("#back-to-top").addClass('active');
-   } else {
-     $("#back-to-top").removeClass('active');
-   }
-});
+// $(window).scroll(function () {
+//     var top=$(window).scrollTop();
+//    if (top > 100) {
+//      $("#back-to-top").addClass('active');
+//    } else {
+//      $("#back-to-top").removeClass('active');
+//    }
+// });
 
-$(document).ready(function(){
-    $("#back-to-top").click(function () {
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-        return false;
-    });
+// $(document).ready(function(){
+//     $("#back-to-top").click(function () {
+//         $("html, body").animate({ scrollTop: 0 }, "slow");
+//         return false;
+//     });
+// });
+
+$('#openmaps').click(function(){
+
+     $('.maps').addClass('maps2');
+     
 });
